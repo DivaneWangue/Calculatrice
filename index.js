310d4968fc879e8f4e05bfbb442851517b5b8130
MaelBallereau <ballereaumael@gmail.com>
2026-03-12 08:58:04 +0100

    feature: use addition

---
+++ index.js
@@ -0,0 +1,15 @@
+import { addition } from './function/addition.js';
+import readline from 'readline';
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question('Premier nombre : ', (a) => {
+    rl.question('Deuxième nombre : ', (b) => {
+        const result = addition(parseFloat(a), parseFloat(b));
+        console.log('Résultat :', result);
+        rl.close();
+    });
+});
